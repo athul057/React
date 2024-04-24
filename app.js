04e60b898ffd57945520b84d408c87b1abbaf3d7
athul <athuli057@gmail.com>
2024-04-24 11:00:56 +0530

    added commets

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-const mainroot = React.createElement('div', { id: 'mainroot' },
+// const mainroot = React.createElement('div', { id: 'mainroot' },
 
- React.createElement('div', { id: 'child' },
-  [React.createElement('h1', {}, "I am H12"),
-  React.createElement('h2', {}, "I am H22")]
+//  React.createElement('div', { id: 'child' },
+//   [React.createElement('h1', {}, "I am H12"),
+//   React.createElement('h2', {}, "I am H22")]
 
 
- )
-)
+//  )
+// )
+
+const mainroot = React.createElement('h1', { id: 'heading' }, 'hello world')
 const main = ReactDOM.createRoot(document.getElementById('root'));
 main.render(mainroot);
 
