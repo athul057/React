4732a302760286439651116e68d1146c22975131
athul <athuli057@gmail.com>
2024-04-25 11:31:37 +0530

    added commets

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-// const mainroot = React.createElement('div', { id: 'mainroot' },
-
-//  React.createElement('div', { id: 'child' },
-//   [React.createElement('h1', {}, "I am H12"),
-//   React.createElement('h2', {}, "I am H22")]
-
-
-//  )
-// )
 
 const mainroot = React.createElement('h1', { id: 'heading' }, 'hello world')
+
+const jsxHeading = <h1>hello world from jsx</h1>
 const main = ReactDOM.createRoot(document.getElementById('root'));
-main.render(mainroot);
 
 
+main.render(jsxHeading);
 
 
