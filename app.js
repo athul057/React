052a42f9f3ab256fcafd18ffcf16b5307d2db648
athul <athuli057@gmail.com>
2024-04-27 11:17:24 +0530

    added commets

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ import ReactDOM from 'react-dom/client'
 
 const mainroot = React.createElement('h1', { id: 'heading' }, 'hello world')
 
-const jsxHeading = <h1>hello world from jsx</h1>
+const heading = <h1>hello world from jsx</h1>
 const main = ReactDOM.createRoot(document.getElementById('root'));
-
-
-main.render(jsxHeading);
+main.render(heading);
 
 
