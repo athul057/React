3bf5e6e7562bcb1d53823a2f81f634eaa38a46eb
athul <athuli057@gmail.com>
2024-05-27 12:26:48 +0530

    useeffect hook

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header';
 import Card from './components/Card';
@@ -11,8 +11,18 @@ import { useState } from 'react';
 
 //BODY
 const Body = () => {
-  const [restData, useRestData] = useState(rest)
+  const [restData, useRestData] = useState([])
 
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.91850&lng=76.25580&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+    const myData = await data.json();
+    console.log(myData);
+    useRestData(myData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
   return (<>
     <button onClick={() => {
       const myData = restData.filter((data) => {
