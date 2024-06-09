03adf0d7ab8b267f42182b46ebd16b7098c4eb60
athul <athuli057@gmail.com>
2024-06-09 18:22:00 +0530

    react-router

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,4 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
+
 const Error = () => {
- return <h1>Ooops Some error occured....</h1>
+
+ const err = useRouteError();
+ console.log(err);
+ return (<>
+  <h1>Ooops Some error occured....</h1>
+  <h3>{err.status} {err.statusText}</h3>
+
+ </>
+
+
+ )
 }
 export default Error;
